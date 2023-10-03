b79771bf574a43e5add050db8040e75c85a10c8d
ayarabia <ayarabia732@gmail.com>
2023-10-03 20:37:05 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center text-Secondary">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
